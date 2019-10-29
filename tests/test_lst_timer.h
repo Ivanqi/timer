ecc7e72831c38eea29af0561121dae0f6b0389b5
Ivanqi <512350225@qq.com>
2019-10-29 16:26:52 +0800

    【开发】增加升序链表测试案例

---
+++ tests/test_lst_timer.h
@@ -0,0 +1,23 @@
+#ifndef TEST_LIST_TIMER
+#define TEST_LIST_TIMER
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+#include <src/lst_timer.h>
+
+
+void test_lst_timer (const char* ip,int port);
+
+#endif
